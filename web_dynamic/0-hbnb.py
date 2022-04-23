9b8d6891f37b239e2da26bca29a9a1a61f2b1816
NathyG2524 <nathygirma2524@gmail.com>
2022-04-23 13:04:31 +0300

    cache_id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,6 +6,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+import uuid
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -33,8 +34,10 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = uuid.uuid4()
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
+                            cache_id,
                            states=st_ct,
                            amenities=amenities,
                            places=places)
